02f45f03af93b7d5d5b034c47c38dbc1911026af
brunodecruz <brunodecruz.05@gmail.com>
2024-01-17 10:03:39 -0300

    v1.1.1 getting data from dictionary

--- plugins/reports.js
+++ plugins/reports.js
@@ -15,4 +15,24 @@ function Reports() {
     button.appendChild(text);
 
     headSection.appendChild(button);
+
+    //oon click  i want to get the two dates the one ate the beggining of the day and the current date
+
+    button.addEventListener('click', function() {
+        let startDate = new Date();
+        startDate.setHours(0,0,0,0);
+        let endDate = new Date();
+        
+        http.get('/ros2dictionary.json')
+        .then(function (result) {
+            result.data.measurements
+            .forEach(element => {
+                console.log(element.key)
+            });
+        });
+        //get the dictionary json
+
+    });
+
+
 }
