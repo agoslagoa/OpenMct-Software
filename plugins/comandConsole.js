d04b9094a54185a1fcb54a568657bda475bee8c5
brunodecruz <brunodecruz.05@gmail.com>
2024-02-05 01:38:20 -0300

    v1.2.6

--- plugins/comandConsole.js
+++ plugins/comandConsole.js
@@ -1,6 +1,8 @@
 function ComandConsole(){
     const commands =['w','s','a','d','x']
     // now i need to hear  if they press a key that is in the commands or its uppercase variation
+    const button = document.createElement('button');
+    button.classList.add('console-button')
     // i need to eliminate this element  <div class="l-pane l-pane--vertical-handle-before"> and their childs
     const lPane = document.querySelector('.l-pane.l-pane--vertical-handle-before');
     lPane.remove();
@@ -8,18 +10,26 @@ function ComandConsole(){
     const lMultipane = document.querySelector('.l-multipane.l-multipane--vertical');
     lMultipane.appendChild(button);
     
-    const button = document.createElement('button');
-    button.classList.add('console-button');
-    button.innerHTML = 'Open Console';
-    let open = false
-    button.addEventListener('click', function(){
-        if(!open){
-            button.innerHTML = 'Open Console';
-        }else{
-            open=true;
-            button.innerHTML = 'Close Console';
+    let icon = document.createElement('i');
+    icon.classList.add('fa-solid');
+    icon.classList.add('fa-gamepad');
+    
+    let text = document.createElement('span');
+    text.innerHTML = 'Open Console';
+
+    button.appendChild(icon);
+    button.appendChild(text);
+    
+    let open = false;
+    button.addEventListener('click', function() {
+        if (open) {
+            open = false;
+            text.innerHTML = 'Open Console';
+        } else {
+            open = true;
+            text.innerHTML = 'Close Console';
         }
-    })
+    });
 
     
     document.addEventListener('keydown', function(event){
@@ -33,4 +43,17 @@ function ComandConsole(){
         }
         
     })
+
+    //i ned to a div to this l-pane l-pane--reacts l-shell__pane-main
+    const lShell = document.querySelector('.l-pane.l-pane--reacts.l-shell__pane-main');
+    const console = document.createElement('div');
+    console.classList.add('console-container');
+    lShell.appendChild(console);
+    //for each element in the commands array i need to create a button
+    commands.forEach(command => {
+        let button = document.createElement('button');
+        button.classList.add('command-button');
+        button.innerHTML = command;
+        console.appendChild(button);
+    });
 }
